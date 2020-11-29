8b6e84ee839d38171a522376bccd1c285d339b0a
AlexWGraven <alex.graven@outlook.com>
2020-11-28 19:29:02 -0500

    Fixes app.js, fixes typo in package.json and adds first failing test

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,1 +1,9 @@
-.
+// app.test.js
+import { should } from "chai";
+import MessageApp from './app.js'
+describe("app", function() {
+  let testApp = new MessageApp
+  it("app has messages", function() {
+    testApp.messages.should.be.an('array');
+  });
+});
